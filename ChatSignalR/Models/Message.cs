59072d59af86f5bca9376eaad6a27fc28301b986
AlauddinSaikat71 <alauddin27@student.sust.edu>
2021-01-09 01:08:58 +0600

    Message and appuser model

---
+++ ChatSignalR/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSignalR.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        [Required]
+        public String UserName { get; set; }
+        [Required]
+        public String Text { get; set; }
+        public DateTime When { get; set; }
+        public int UserID { get; set; }
+        public virtual AppUser AppUser { get; set; }
+    }
+}
